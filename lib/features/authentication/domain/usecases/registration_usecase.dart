7209894c657b0852e99c6c03b780f08aa235201a
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-28 15:12:33 +0700

    Authentication: add AuthenticationRepository into RegistrationUsecase as a DependencyInjection

--- lib/features/authentication/domain/usecases/registration_usecase.dart
+++ lib/features/authentication/domain/usecases/registration_usecase.dart
@@ -2,9 +2,16 @@ import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/core/usecase/usecase.dart';
 import 'package:crosscheck/features/authentication/data/models/request/registration_params.dart';
 import 'package:crosscheck/features/authentication/domain/entities/authentication_entity.dart';
+import 'package:crosscheck/features/authentication/domain/repositories/authentication_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class RegistrationUsecase implements Usecase<AuthenticationEntity, RegistrationParams>{
+class RegistrationUsecase implements Usecase<AuthenticationEntity, RegistrationParams> {
+  final AuthenticationRepository repository;
+
+  RegistrationUsecase({
+    required this.repository
+  });
+
   @override
   Future<Either<Failure, AuthenticationEntity>>? call(RegistrationParams param) {
     return null;
