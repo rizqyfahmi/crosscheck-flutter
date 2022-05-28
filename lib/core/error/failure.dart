dad8bef533d28ef35e5e5ef634b3b322a40c0c82
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-28 19:31:42 +0700

    Core: specialize the failure into ServerFailure and CachedFailure

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,6 +1,33 @@
 import 'package:equatable/equatable.dart';
 
+/* 
+  Failure classes are only focus on error that is resulted by "Either"
+*/
 abstract class Failure extends Equatable {
   @override
   List<Object?> get props => [];
+}
+
+class ServerFailure extends Failure {
+  final String message;
+  final List<Map<String, dynamic>>? errors;
+
+  ServerFailure({
+    required this.message,
+    this.errors
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class CachedFailure extends Failure {
+  final String message;
+
+  CachedFailure({
+    required this.message
+  });
+
+  @override
+  List<Object?> get props => [message];
 }
