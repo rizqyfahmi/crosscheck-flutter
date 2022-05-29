8e78d9109b2dc4824e9f658d9267a0ad3b409497
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-29 07:10:04 +0700

    Authentication: solve nullable returned value test of registration and setToken

--- lib/features/authentication/domain/usecases/registration_usecase.dart
+++ lib/features/authentication/domain/usecases/registration_usecase.dart
@@ -16,7 +16,11 @@ class RegistrationUsecase implements Usecase<AuthenticationEntity, RegistrationP
   Future<Either<Failure, AuthenticationEntity>>? call(RegistrationParams params) async {
     final result = await repository.registration(params);
 
-    return result!.fold(
+    if (result == null) {
+      return Left(NullFailure());
+    }
+
+    return result.fold(
       (error) => Left(error), 
       (success) async => await _setToken(success)
     );
@@ -25,7 +29,11 @@ class RegistrationUsecase implements Usecase<AuthenticationEntity, RegistrationP
   Future<Either<Failure, AuthenticationEntity>> _setToken(AuthenticationEntity entity) async {
     final result = await repository.setToken(entity.token);
     
-    return result!.fold(
+    if (result == null) {
+      return Left(NullFailure());
+    }
+
+    return result.fold(
       (error) => Left(error),
       (_) => Right(entity)
     );
