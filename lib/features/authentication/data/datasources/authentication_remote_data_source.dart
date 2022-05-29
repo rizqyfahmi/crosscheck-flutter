e09bb7389a392f31173a759855ca80d987d4849d
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-29 14:40:15 +0700

    Authentication: refactor code for remote data source

--- lib/features/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/features/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,7 +1,8 @@
 import 'package:crosscheck/features/authentication/data/models/data/authentication_model.dart';
+import 'package:crosscheck/features/authentication/data/models/request/registration_params.dart';
 
 abstract class AuthenticationRemoteDataSource {
   
-  Future<AuthenticationModel> registration();
+  Future<AuthenticationModel> registration(RegistrationParams params);
   
 }
