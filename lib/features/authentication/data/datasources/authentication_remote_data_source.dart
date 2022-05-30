fd613fa6e2fb064ef7ae6bae2937a4e68f93c018
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-30 07:30:14 +0700

    Authentication: add client field into remote data source

--- lib/features/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/features/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,6 +1,6 @@
-import 'package:crosscheck/features/authentication/data/models/data/authentication_model.dart';
 import 'package:crosscheck/features/authentication/data/models/request/registration_params.dart';
 import 'package:crosscheck/features/authentication/data/models/response/authentication_response_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class AuthenticationRemoteDataSource {
   
@@ -9,6 +9,12 @@ abstract class AuthenticationRemoteDataSource {
 }
 
 class AuthenticationRemoteDataSourceImpl implements AuthenticationRemoteDataSource {
+
+  final http.Client client;
+
+  AuthenticationRemoteDataSourceImpl({
+    required this.client
+  });
   
   @override
   Future<AuthenticationResponseModel> registration(RegistrationParams params) {
