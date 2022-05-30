033164e5b086ada36ccd6b22e5309ffa4b1b7895
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-30 08:42:18 +0700

    Authentication: refactor authentication response model

--- lib/core/response/response.dart
+++ lib/core/response/response.dart
@@ -1,9 +1,14 @@
-abstract class Response {
+import 'package:equatable/equatable.dart';
+
+class Response extends Equatable{
   final String message;
   final dynamic data;
 
-  Response({
+  const Response({
     required this.message,
     required this.data
   });
+
+  @override
+  List<Object?> get props => [message, data];
 }
--- lib/features/authentication/data/models/response/authentication_response_model.dart
+++ lib/features/authentication/data/models/response/authentication_response_model.dart
@@ -1,16 +1,16 @@
 import 'package:crosscheck/core/response/response.dart';
 import 'package:crosscheck/features/authentication/data/models/data/authentication_model.dart';
 
-class AuthenticationResponseModel implements Response {
+class AuthenticationResponseModel extends Response {
   @override
   final String message;
   @override
   final AuthenticationModel data;
 
-  AuthenticationResponseModel({
+  const AuthenticationResponseModel({
     required this.message,
     required this.data
-  });
+  }) : super(message: message, data: data);
 
   factory AuthenticationResponseModel.fromJSON(Map<String, dynamic> response) {
     return AuthenticationResponseModel(
