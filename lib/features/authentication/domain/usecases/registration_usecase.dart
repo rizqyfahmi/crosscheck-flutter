57f4977a1cbdcedc326e29bb7a13972e1ab3dc70
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-05-31 18:50:04 +0700

    Authentication: refactor code for the usecase

--- lib/features/authentication/domain/usecases/registration_usecase.dart
+++ lib/features/authentication/domain/usecases/registration_usecase.dart
@@ -13,7 +13,7 @@ class RegistrationUsecase implements Usecase<AuthenticationEntity, RegistrationP
   });
 
   @override
-  Future<Either<Failure, AuthenticationEntity>>? call(RegistrationParams params) async {
+  Future<Either<Failure, AuthenticationEntity>> call(RegistrationParams params) async {
     final result = await repository.registration(params);
 
     if (result == null) return Left(NullFailure());
