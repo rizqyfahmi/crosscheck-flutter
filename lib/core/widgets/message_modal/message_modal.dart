53ff72c8407fedf9f88ee050fdbb64955f6acf84
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-05 10:02:07 +0700

    Core: create a new widget that is used to display error and success modal

---
+++ lib/core/widgets/message_modal/message_modal.dart
@@ -0,0 +1,211 @@
+import 'package:crosscheck/assets/colors/custom_colors.dart';
+import 'package:crosscheck/assets/icons/crosscheck/crosscheck_icons.dart';
+import 'package:crosscheck/core/widgets/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+enum MessageModalStatus {
+  success,
+  error
+}
+
+class MessageModal extends StatelessWidget {
+  final Widget child;
+  final MessageModalStatus status;
+  final String? title;
+  final String message;
+  final bool isShown;
+
+  const MessageModal({
+    Key? key,
+    required this.status,
+    required this.child,
+    this.title,
+    required this.message,
+    this.isShown = false
+  }) : super(key: key);
+
+  Widget buildTitle(BuildContext context) {
+    final tempTitle = title ?? (status == MessageModalStatus.error ? "Error!" : "Congratulations!");
+
+    return Column(
+      children: [
+        Text(
+          tempTitle,
+          textAlign: TextAlign.center,
+          style: TextStyles.poppinsBold18.copyWith(
+            color: CustomColors.ternary
+          )
+        ),
+        const SizedBox(height: 16)
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final color = status == MessageModalStatus.error ? CustomColors.primary : CustomColors.success;
+    final icon = status == MessageModalStatus.error ? Crosscheck.cross : Crosscheck.check;
+    
+    if (!isShown) {
+      return child;
+    }
+
+    return Stack(
+      children: [
+        child,
+        Positioned(
+          child: Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      child: LayoutBuilder(
+                        builder: (context, contraints) {
+                          final size = contraints.maxWidth / 1.5;
+                          return Stack(
+                            children: [
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    height: size / 2,
+                                    width: size
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          clipBehavior: Clip.antiAlias,
+                                          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 32),
+                                          decoration: const BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.all(Radius.circular(12))
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                top: -(size/2),
+                                                left: 0,
+                                                right: 0,
+                                                child: Center(
+                                                  child: Container(
+                                                    height: size,
+                                                    width: size,
+                                                    padding: const EdgeInsets.all(16),
+                                                    decoration: BoxDecoration(
+                                                      shape: BoxShape.circle,
+                                                      color: color.withOpacity(0.2)
+                                                    ),
+                                                    child: Container(
+                                                      height: size,
+                                                      width: size,
+                                                      padding: const EdgeInsets.all(16),
+                                                      decoration: BoxDecoration(
+                                                        shape: BoxShape.circle,
+                                                        color: color.withOpacity(0.2)
+                                                      ),
+                                                      child: Container(
+                                                        height: size,
+                                                        width: size,
+                                                        decoration: BoxDecoration(
+                                                          shape: BoxShape.circle,
+                                                          color: color.withOpacity(0.2)
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              Center(
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children: [
+                                                    SizedBox(
+                                                      height: size / 2,
+                                                      width: size
+                                                    ),
+                                                    const SizedBox(height: 32),
+                                                    buildTitle(context),
+                                                    Text(
+                                                      message,
+                                                      textAlign: TextAlign.center,
+                                                      style: TextStyles.poppinsMedium14
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Positioned(
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                child: Center(
+                                  child: Container(
+                                    height: size,
+                                    width: size,
+                                    padding: const EdgeInsets.all(16),
+                                    child: Container(
+                                      height: size,
+                                      width: size,
+                                      padding: const EdgeInsets.all(16),
+                                      child: Container(
+                                        height: size,
+                                        width: size,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: color
+                                        ),
+                                        child: Icon(icon, color: Colors.white, size: 128),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 16,
+                      margin: const EdgeInsets.symmetric(horizontal: 48),
+                      decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(12), 
+                          bottomRight: Radius.circular(12)
+                        )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+
+  }
+}
