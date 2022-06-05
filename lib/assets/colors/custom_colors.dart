616ac10cb615b2e500a6c43370a445dc4ba8d609
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-05 10:00:28 +0700

    Core: add a new color for success state

--- lib/assets/colors/custom_colors.dart
+++ lib/assets/colors/custom_colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
+  
+  CustomColors._();
+
   static const primary = Color.fromRGBO(242, 75, 89, 1);
   static const secondary = Color.fromRGBO(32, 58, 64, 1);
   static const ternary = Color.fromRGBO(161, 164, 178, 1.00);
   static const placeholderText = Color.fromRGBO(170, 182, 190, 1);
   static const borderField = Color.fromRGBO(242, 242, 242, 1);
+  static const success = Color.fromRGBO(0, 172, 124, 1);
 }
