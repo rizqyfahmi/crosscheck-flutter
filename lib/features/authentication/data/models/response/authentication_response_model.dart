18df49fc7320a45d4e94b4a391b2db297b487816
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-06 14:20:45 +0700

    Authentication: remove overide fields on authentication response model

--- lib/features/authentication/data/models/response/authentication_response_model.dart
+++ lib/features/authentication/data/models/response/authentication_response_model.dart
@@ -2,15 +2,11 @@ import 'package:crosscheck/core/response/response.dart';
 import 'package:crosscheck/features/authentication/data/models/data/authentication_model.dart';
 
 class AuthenticationResponseModel extends Response {
-  @override
-  final String message;
-  @override
-  final AuthenticationModel data;
-
+  
   const AuthenticationResponseModel({
-    required this.message,
-    required this.data
-  }) : super(message: message, data: data);
+    required super.message,
+    super.data
+  });
 
   factory AuthenticationResponseModel.fromJSON(Map<String, dynamic> response) {
     return AuthenticationResponseModel(
