557cb8a144f56bcdd26d12f349d2e89e16a27f33
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-06 20:58:03 +0700

    Test: create some test cases for login usecase in authentication remote data source

--- test/features/authentication/data/datasource/authentication_remote_data_source_test.dart
+++ test/features/authentication/data/datasource/authentication_remote_data_source_test.dart
@@ -1,6 +1,7 @@
 import 'package:crosscheck/core/error/exception.dart';
 import 'package:crosscheck/features/authentication/data/datasources/authentication_remote_data_source.dart';
 import 'package:crosscheck/features/authentication/data/models/data/authentication_model.dart';
+import 'package:crosscheck/features/authentication/data/models/request/login_params.dart';
 import 'package:crosscheck/features/authentication/data/models/request/registration_params.dart';
 import 'package:crosscheck/features/authentication/data/models/response/authentication_response_model.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,6 +17,7 @@ void main() {
   late MockClient mockClient;
   late AuthenticationRemoteDataSource remoteDataSource;
   late RegistrationParams registrationParams;
+  late LoginParams loginParams;
 
   // Mock Result
   const String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
@@ -25,6 +27,7 @@ void main() {
     mockClient = MockClient();
     remoteDataSource = AuthenticationRemoteDataSourceImpl(client: mockClient);
     registrationParams = const RegistrationParams(name: "Fulan", email: "fulan@email.com", password: "fulan123", confirmPassword: "fulan123");
+    loginParams = const LoginParams(username: "fulan@email.com", password: "Password123");
   });
 
   test("Should return AuthenticationResponseModel", () async {
@@ -40,6 +43,23 @@ void main() {
     when(mockClient.get(any, headers: {"Content-Type": "application/json"})).thenAnswer((_) async => http.Response("Something went wrong", 400));
     
     final call = remoteDataSource.registration(registrationParams);
+
+    expect(() => call, throwsA(const TypeMatcher<ServerException>()));
+  });
+
+  test("Should return AuthenticationResponseModel when login is success", () async {
+    final String filepath = stringify("test/features/authentication/data/models/response/authentication_response.json");
+    when(mockClient.get(any, headers: {"Content-Type": "application/json"})).thenAnswer((_) async => http.Response(filepath, 200));
+
+    final result = await remoteDataSource.login(loginParams);
+
+    expect(result, equals(responseModel));
+  });
+
+  test("Should throw a ServerException when login is failed because of 500", () async {
+    when(mockClient.get(any, headers: {"Content-Type": "application/json"})).thenAnswer((_) async => http.Response("Something went wrong", 500));
+    
+    final call = remoteDataSource.login(loginParams);
 
     expect(() => call, throwsA(const TypeMatcher<ServerException>()));
   });
