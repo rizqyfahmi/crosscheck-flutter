a57ce420364ebbc5deb2d6569b2deb261e86859e
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-06 19:24:51 +0700

    Test: add some test cases for login usecase

--- test/features/authentication/domain/usecases/login_usecase_test.dart
+++ test/features/authentication/domain/usecases/login_usecase_test.dart
@@ -1,3 +1,4 @@
+import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/features/authentication/data/models/request/login_params.dart';
 import 'package:crosscheck/features/authentication/domain/entities/authentication_entity.dart';
 import 'package:crosscheck/features/authentication/domain/repositories/authentication_repository.dart';
@@ -25,7 +26,7 @@ void main() {
     loginParams = const LoginParams(username: "fulan@email.com", password: "Password123");
   });
 
-  test("Should get the authentication token after login is success", () async {
+  test("Should get the authentication token when login is success", () async {
     when(mockAuthenticationRepository.login(loginParams)).thenAnswer((_) async => const Right(authenticationEntity));
 
     final result = await usecase(loginParams);
@@ -34,4 +35,40 @@ void main() {
     verify(mockAuthenticationRepository.login(loginParams));
   });
 
+  test("Should cache the authentication token when login success", () async {
+    when(mockAuthenticationRepository.login(loginParams)).thenAnswer((_) async => const Right(authenticationEntity));
+
+    final result = await usecase(loginParams);
+
+    expect(result, const Right(AuthenticationEntity(token: token)));
+    verify(mockAuthenticationRepository.setToken(token));
+  });
+
+  test("Should not get the authentication when login is failed", () async {
+    when(mockAuthenticationRepository.login(loginParams)).thenAnswer((_) async => Left(ServerFailure(message: "Something went wrong")));
+
+    final result = await usecase(loginParams);
+    
+    expect(result, isNot(const Right(AuthenticationEntity(token: token))));
+    verify(mockAuthenticationRepository.login(loginParams));
+  });
+
+  test("Should not cache the authentication when login is failed", () async {
+    when(mockAuthenticationRepository.login(loginParams)).thenAnswer((_) async => Left(ServerFailure(message: "Something went wrong")));
+
+    final result = await usecase(loginParams);
+    
+    expect(result, isNot(const Right(AuthenticationEntity(token: token))));
+    verifyNever(mockAuthenticationRepository.setToken(token));
+  });
+
+  test("Should get a failure as the same as the failure that is returned when login is failed", () async {
+    when(mockAuthenticationRepository.login(loginParams)).thenAnswer((_) async => Left(ServerFailure(message: "Something went wrong")));
+
+    final result = await usecase(loginParams);
+    
+    expect(result, Left(ServerFailure(message: "Something went wrong")));
+    verify(mockAuthenticationRepository.login(loginParams));
+    verifyNever(mockAuthenticationRepository.setToken(token));
+  });
 }
