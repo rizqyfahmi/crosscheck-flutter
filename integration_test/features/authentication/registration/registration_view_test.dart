b1774e7fb15c2e282fefc24ecf8a2e23ea3e3e8b
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-06 10:38:56 +0700

    Test: add more expectation into integration test of registration view

--- integration_test/features/authentication/registration/registration_view_test.dart
+++ integration_test/features/authentication/registration/registration_view_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:crosscheck/main.dart' as app;
@@ -10,5 +11,13 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text("Create your account"), findsOneWidget);
+    expect(find.byKey(const Key("logo")), findsOneWidget);
+    expect(find.byKey(const Key("nameField")), findsOneWidget);
+    expect(find.byKey(const Key("emailField")), findsOneWidget);
+    expect(find.byKey(const Key("passwordField")), findsOneWidget);
+    expect(find.byKey(const Key("confirmPasswordField")), findsOneWidget);
+    expect(find.byKey(const Key("submitButton")), findsOneWidget);
+    expect(find.byKey(const Key("signInTextButton")), findsOneWidget);
+    expect(find.byKey(const Key("tncTextButton")), findsOneWidget);
   });
 }
