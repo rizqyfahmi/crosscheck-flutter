a0bbc298e2dd4ea825abe897d61076acce207423
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-07 12:20:16 +0700

    Authentication: solve a validation error test case for login usecase

--- lib/features/authentication/domain/usecases/login_usecase.dart
+++ lib/features/authentication/domain/usecases/login_usecase.dart
@@ -1,9 +1,11 @@
 import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/core/usecase/usecase.dart';
+import 'package:crosscheck/features/authentication/data/models/request/login_params.dart';
+import 'package:crosscheck/features/authentication/domain/entities/authentication_entity.dart';
 import 'package:crosscheck/features/authentication/domain/repositories/authentication_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class LoginUsecase implements Usecase {
+class LoginUsecase implements Usecase<AuthenticationEntity, LoginParams> {
   final AuthenticationRepository repository;
 
   LoginUsecase({
@@ -11,7 +13,11 @@ class LoginUsecase implements Usecase {
   });
 
   @override
-  Future<Either<Failure, dynamic>> call(params) async {
+  Future<Either<Failure, AuthenticationEntity>> call(params) async {
+
+    if (params.username == "" || params.password == "") {
+      return Left(ServerFailure(message: Failure.loginRequiredFieldError));
+    }
     
     final response = await repository.login(params);
 
