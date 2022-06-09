8afb8228c74b4ad3a21a245bb9cf4f5728525a4b
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-09 18:42:02 +0700

    Test: create test case for get "isSkip" usecase in implementation of walkthrough repository

--- test/features/walkthrough/data/repositories/walkthrough_repository_impl_test.dart
+++ test/features/walkthrough/data/repositories/walkthrough_repository_impl_test.dart
@@ -1,8 +1,10 @@
 import 'package:crosscheck/core/error/exception.dart';
 import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/features/walkthrough/data/datasource/walkthrough_local_data_source.dart';
+import 'package:crosscheck/features/walkthrough/data/models/data/walkthrough_model.dart';
 import 'package:crosscheck/features/walkthrough/data/models/request/walkthrough_params.dart';
 import 'package:crosscheck/features/walkthrough/data/repositories/walkthrough_repository_impl.dart';
+import 'package:crosscheck/features/walkthrough/domain/entities/walkthrough_entitiy.dart';
 import 'package:crosscheck/features/walkthrough/domain/repositories/walkthrough_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -41,5 +43,25 @@ void main() {
 
     expect(result, Left(CachedFailure(message: Failure.generalError)));
     verify(mockWalkthroughLocalDataSource.setIsSkip(params));
+  });
+
+  test("Should get \"isSkip\" status properly ", () async {
+    WalkthroughEntity walkthroughEntity = const WalkthroughModel(isSkip: true);
+
+    when(mockWalkthroughLocalDataSource.getIsSkip()).thenAnswer((_) async => const WalkthroughModel(isSkip: true));
+
+    final result = await walkthroughRepository.getIsSkip();
+
+    expect(result, Right(walkthroughEntity));
+    verify(mockWalkthroughLocalDataSource.getIsSkip());
+  });
+
+  test("Should not get \"is skip\" status and return CachedFailure when error is happened", () async {
+    when(mockWalkthroughLocalDataSource.getIsSkip()).thenThrow(CacheException(message: Failure.generalError));
+
+    final result = await walkthroughRepository.getIsSkip();
+
+    expect(result, Left(CachedFailure(message: Failure.generalError)));
+    verify(mockWalkthroughLocalDataSource.getIsSkip());
   });
 }
