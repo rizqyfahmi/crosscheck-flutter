788ca38cc68339b022f172488cfcd1f84062df36
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-09 17:17:31 +0700

    Walkthrough: create get "isSkip" usecase

---
+++ lib/features/walkthrough/domain/usecases/get_is_skip_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:crosscheck/core/error/failure.dart';
+import 'package:crosscheck/core/param/param.dart';
+import 'package:crosscheck/core/usecase/usecase.dart';
+import 'package:crosscheck/features/walkthrough/domain/entities/walkthrough_entitiy.dart';
+import 'package:crosscheck/features/walkthrough/domain/repositories/walkthrough_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetIsSkipUsecase implements Usecase<WalkthroughEntity, NoParam> {
+
+  final WalkthroughRepository repository;
+
+  GetIsSkipUsecase({
+    required this.repository
+  });
+
+  @override
+  Future<Either<Failure, WalkthroughEntity>> call(NoParam param) async {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+  
+}
