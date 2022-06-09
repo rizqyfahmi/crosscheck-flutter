bb603121758d43d71d23dd42dc4d5c0bbd3f5bdc
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-09 21:45:44 +0700

    Walkthrough: create a BLoC of walkthrough

---
+++ lib/features/walkthrough/presentation/view_models/walkthrough_state.dart
@@ -0,0 +1,56 @@
+
+import 'package:crosscheck/features/walkthrough/presentation/view_models/walkthrough_model.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class WalkthroughState extends Equatable {
+  
+  final WalkthroughModel model;
+
+  const WalkthroughState({
+    required this.model
+  });
+
+  @override
+  List<Object?> get props => [model];
+
+}
+
+class WalkthroughInitial extends WalkthroughState {
+  
+  const WalkthroughInitial() : super(model: const WalkthroughModel(isSkip: false));
+  
+}
+
+class WalkthroughSkipSuccess extends WalkthroughState {
+  
+  const WalkthroughSkipSuccess({required super.model});
+
+}
+
+class WalkthroughSkipFailed extends WalkthroughState {
+
+  final String message;
+  
+  const WalkthroughSkipFailed({
+    required super.model,
+    required this.message
+  });
+
+}
+
+class WalkthroughLoadSkipSuccess extends WalkthroughState {
+  
+  const WalkthroughLoadSkipSuccess({required super.model});
+
+}
+
+class WalkthroughLoadSkipFailed extends WalkthroughState {
+
+  final String message;
+  
+  const WalkthroughLoadSkipFailed({
+    required super.model,
+    required this.message
+  });
+
+}
