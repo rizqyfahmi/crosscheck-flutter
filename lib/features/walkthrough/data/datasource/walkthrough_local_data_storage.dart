35414da51fc1ab4a3b30c3793124b9585137f217
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-09 16:29:48 +0700

    Walkthrough: solve test case for set "isSkip" usecase in walkthrough local data source

--- lib/features/walkthrough/data/datasource/walkthrough_local_data_storage.dart
+++ lib/features/walkthrough/data/datasource/walkthrough_local_data_storage.dart
@@ -1,4 +1,8 @@
 
+import 'package:crosscheck/core/error/exception.dart';
+import 'package:crosscheck/core/error/failure.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 abstract class WalkthroughLocalDataSource {
   
   Future<void> setIsSkip(bool isSkip);
@@ -6,11 +10,20 @@ abstract class WalkthroughLocalDataSource {
 }
 
 class WalkthroughLocalDataSourceImpl implements WalkthroughLocalDataSource {
+
+  final SharedPreferences sharedPreferences;
+
+  WalkthroughLocalDataSourceImpl({
+    required this.sharedPreferences
+  });
   
   @override
-  Future<void> setIsSkip(bool isSkip) {
-    // TODO: implement setIsSkip
-    throw UnimplementedError();
+  Future<void> setIsSkip(bool isSkip) async {
+    final response = await sharedPreferences.setBool("isSkip", isSkip);
+
+    if (!response) {
+      throw CacheException(message: Failure.cacheError);
+    }
   }
   
 }
