182c56447366bbd6e65887afe316bbcef8cf13f3
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-11 21:14:30 +0700

    Test: refactor stream test cases for walkthrough view

--- test/features/walkthrough/presentation/view/walkthrough_view_test.dart
+++ test/features/walkthrough/presentation/view/walkthrough_view_test.dart
@@ -82,7 +82,7 @@ void main() {
       testWidget = buildWidget(
         loginBloc: mockLoginBloc,
         walkthroughBloc: mockWalkthroughBloc,
-        child: const WalkthroughView()
+        child: const MainPage()
       );
 
       when(mockLoginBloc.state).thenReturn(const LoginInitial());
@@ -94,36 +94,62 @@ void main() {
       when(mockWalkthroughBloc.stream).thenAnswer((_) => Stream.fromIterable([
         const WalkthroughLoadSkipFailed(message: Failure.cacheError, model: WalkthroughModel(isSkip: true)),
         const WalkthroughSkipSuccess(model: WalkthroughModel(isSkip: true))
-      ]));
+      ]).asyncExpand((state) async* {
+        yield state;
+        await Future.delayed(const Duration(seconds: 1));
+      }));
       
-      await tester.pumpWidget(testWidget);
-      await tester.pumpAndSettle();
+      await tester.runAsync(() async {
+        await tester.pumpWidget(testWidget);
+        await tester.pumpAndSettle();
 
-      expect(find.byKey(const Key("logo")), findsOneWidget);
-      expect(find.byKey(const Key("usernameField")), findsOneWidget);
-      expect(find.byKey(const Key("passwordField")), findsOneWidget);
-      expect(find.byKey(const Key("submitButton")), findsOneWidget);
-      expect(find.byKey(const Key("forgotPasswordTextButton")), findsOneWidget);
-      expect(find.byKey(const Key("signUpTextButton")), findsOneWidget);
+        expect(find.byKey(const Key("illustration")), findsOneWidget);
+        expect(find.text("Welcome"), findsOneWidget);
+        expect(find.text("Focus on your short and long-term habit to improve productivity and achieve your goals. Enjoy your way to better time management"), findsOneWidget);
+        expect(find.byKey(const Key("getStartedButton")), findsOneWidget);
 
+        await Future.delayed(const Duration(seconds: 1));
+        await tester.pumpAndSettle();
+
+        expect(find.byKey(const Key("logo")), findsOneWidget);
+        expect(find.byKey(const Key("usernameField")), findsOneWidget);
+        expect(find.byKey(const Key("passwordField")), findsOneWidget);
+        expect(find.byKey(const Key("submitButton")), findsOneWidget);
+        expect(find.byKey(const Key("forgotPasswordTextButton")), findsOneWidget);
+        expect(find.byKey(const Key("signUpTextButton")), findsOneWidget);
+      });
     });
 
     testWidgets("Should redirect to login view properly when SetIsSkipUsecase returns WalkthroughSkipFailed", (WidgetTester tester) async {
       when(mockWalkthroughBloc.state).thenReturn(const WalkthroughInitial());
       when(mockWalkthroughBloc.stream).thenAnswer((_) => Stream.fromIterable([
         const WalkthroughLoadSkipFailed(message: Failure.cacheError, model: WalkthroughModel(isSkip: true)),
-        const WalkthroughSkipFailed(message: Failure.cacheError, model: WalkthroughModel(isSkip: true))
-      ]));
+        const WalkthroughSkipFailed(message: Failure.cacheError, model: WalkthroughModel(isSkip: false))
+      ]).asyncExpand((state) async* {
+        yield state;
+        await Future.delayed(const Duration(seconds: 1));
+      }));
       
-      await tester.pumpWidget(testWidget);
-      await tester.pumpAndSettle();
+      await tester.runAsync(() async {
+        await tester.pumpWidget(testWidget);
+        await tester.pumpAndSettle();
 
-      expect(find.byKey(const Key("logo")), findsOneWidget);
-      expect(find.byKey(const Key("usernameField")), findsOneWidget);
-      expect(find.byKey(const Key("passwordField")), findsOneWidget);
-      expect(find.byKey(const Key("submitButton")), findsOneWidget);
-      expect(find.byKey(const Key("forgotPasswordTextButton")), findsOneWidget);
-      expect(find.byKey(const Key("signUpTextButton")), findsOneWidget);
+        expect(find.byKey(const Key("illustration")), findsOneWidget);
+        expect(find.text("Welcome"), findsOneWidget);
+        expect(find.text("Focus on your short and long-term habit to improve productivity and achieve your goals. Enjoy your way to better time management"), findsOneWidget);
+        expect(find.byKey(const Key("getStartedButton")), findsOneWidget);
+
+        await Future.delayed(const Duration(seconds: 1));
+        await tester.pumpAndSettle();
+
+        expect(find.byKey(const Key("logo")), findsOneWidget);
+        expect(find.byKey(const Key("usernameField")), findsOneWidget);
+        expect(find.byKey(const Key("passwordField")), findsOneWidget);
+        expect(find.byKey(const Key("submitButton")), findsOneWidget);
+        expect(find.byKey(const Key("forgotPasswordTextButton")), findsOneWidget);
+        expect(find.byKey(const Key("signUpTextButton")), findsOneWidget);
+
+      });
 
     });
   });
