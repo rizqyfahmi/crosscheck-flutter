5465f3ff9f0f89ae72984465f0058e188aeb7a1f
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-12 18:07:05 +0700

    Main: create main repository

---
+++ lib/features/main/domain/repositories/main_repository.dart
@@ -0,0 +1,9 @@
+import 'package:crosscheck/core/error/failure.dart';
+import 'package:crosscheck/features/main/domain/entities/bottom_navigation_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class MainRepository {
+  
+  Future<Either<Failure, BottomNavigationEntity>> getActiveBottomNavigation();
+
+}
