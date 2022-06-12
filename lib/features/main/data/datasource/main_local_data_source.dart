0fcda478a2462590054c085e5bb755a795318b43
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-12 21:47:16 +0700

    Main: create implementation of main local data source

--- lib/features/main/data/datasource/main_local_data_source.dart
+++ lib/features/main/data/datasource/main_local_data_source.dart
@@ -1,7 +1,28 @@
+import 'dart:convert';
+
+import 'package:crosscheck/core/error/exception.dart';
+import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/features/main/data/model/bottom_navigation_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class MainLocalDataSource {
   
   Future<BottomNavigationModel> getActiveBottomNavigation();
 
+}
+
+class MainLocalDataSourceImpl implements MainLocalDataSource {
+  
+  final SharedPreferences sharedPreferences;
+
+  const MainLocalDataSourceImpl({
+    required this.sharedPreferences
+  });
+  
+  @override
+  Future<BottomNavigationModel> getActiveBottomNavigation() {
+    // TODO: implement getActiveBottomNavigation
+    throw UnimplementedError();
+  }
+  
 }
