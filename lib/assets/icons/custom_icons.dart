34ccc34069f1f4e247312cefff8cc497b84b15cf
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-13 14:45:59 +0700

    Core: refactor assets

---
+++ lib/assets/icons/custom_icons.dart
@@ -0,0 +1,12 @@
+class CustomIcons {
+  
+  CustomIcons._();
+
+  static const home = "lib/assets/icons/home.svg";
+  static const calendar = "lib/assets/icons/calendar.svg";
+  static const history = "lib/assets/icons/history.svg";
+  static const setting = "lib/assets/icons/setting.svg";
+  static const plus = "lib/assets/icons/plus.svg";
+  static const cross = "lib/assets/icons/cross.svg";
+  static const check = "lib/assets/icons/check.svg";
+}
