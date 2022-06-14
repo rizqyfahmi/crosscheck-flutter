18a02e4cec4ab3c4dadba7466eaca622a0453c10
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-14 16:07:20 +0700

    Dashboard: create implementation of dashboard remote data source

--- lib/features/dashboard/data/datasource/dashboard_remote_data_source.dart
+++ lib/features/dashboard/data/datasource/dashboard_remote_data_source.dart
@@ -1,7 +1,25 @@
+
 import 'package:crosscheck/features/dashboard/data/models/data/dashboard_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class DashboardRemoteDataSource {
   
   Future<DashboardModel> getDashboard(String token);
 
+}
+
+class DashboardRemoteDataSourceImpl implements DashboardRemoteDataSource {
+
+  final http.Client client;
+
+  const DashboardRemoteDataSourceImpl({
+    required this.client
+  });
+  
+  @override
+  Future<DashboardModel> getDashboard(String token) {
+    // TODO: implement getDashboard
+    throw UnimplementedError();
+  }
+  
 }
