888779ad40f92c90a272cbcfe4ade623da9c4480
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-14 11:57:31 +0700

    Dashboard: create usecase and repository

---
+++ lib/features/dashboard/domain/usecases/get_dashboard_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:crosscheck/core/error/failure.dart';
+import 'package:crosscheck/core/usecase/usecase.dart';
+import 'package:crosscheck/features/dashboard/data/models/params/dashboard_params.dart';
+import 'package:crosscheck/features/dashboard/domain/entities/dashboard_entity.dart';
+import 'package:crosscheck/features/dashboard/domain/repositories/dashboard_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetDashboardUsecase implements Usecase<DashboardEntity, DashboardParams> {
+  
+  final DashboardRepository repository;
+
+  GetDashboardUsecase({
+    required this.repository
+  });
+  
+  @override
+  Future<Either<Failure, DashboardEntity>> call(DashboardParams params) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+
+  
+}
