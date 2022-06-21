2457d6e4c3ec6058010f9c534bf52aab4afc1773
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-21 09:27:39 +0700

    Settings: create implementation of local data source for set theme usecase

--- lib/features/settings/data/datasource/settings_local_data_source.dart
+++ lib/features/settings/data/datasource/settings_local_data_source.dart
@@ -1,7 +1,22 @@
 import 'package:crosscheck/features/settings/data/models/data/settings_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class SettingsLocalDataSource {
   
   Future<void> setTheme(SettingsModel params);
 
+}
+
+class SettingsLocalDataSourceImpl implements SettingsLocalDataSource {
+  
+  final SharedPreferences sharedPreferences;
+
+  SettingsLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> setTheme(SettingsModel params) {
+    // TODO: implement setTheme
+    throw UnimplementedError();
+  }
+  
 }
