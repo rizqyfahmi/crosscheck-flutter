91374027e963d6ad3a90631608cc13c4f88a99f1
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-21 09:47:02 +0700

    Settings: solve test case for implementation of settings local data source for set theme usecase

--- lib/features/settings/data/datasource/settings_local_data_source.dart
+++ lib/features/settings/data/datasource/settings_local_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:crosscheck/core/error/exception.dart';
+import 'package:crosscheck/core/error/failure.dart';
 import 'package:crosscheck/features/settings/data/models/data/settings_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -14,9 +18,12 @@ class SettingsLocalDataSourceImpl implements SettingsLocalDataSource {
   SettingsLocalDataSourceImpl({required this.sharedPreferences});
 
   @override
-  Future<void> setTheme(SettingsModel params) {
-    // TODO: implement setTheme
-    throw UnimplementedError();
+  Future<void> setTheme(SettingsModel params) async {
+    final response = await sharedPreferences.setString("CACHED_SETTINGS", json.encode(params.toJSON()));
+
+    if (!response) {
+      throw CacheException(message: Failure.cacheError);
+    }
   }
   
 }
