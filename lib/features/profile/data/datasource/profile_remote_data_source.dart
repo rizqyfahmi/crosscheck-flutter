f5e856561cc836b8a505312a0d15976a07494e63
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-06-29 15:02:48 +0700

    Profile: create implementation of profile remote data source

--- lib/features/profile/data/datasource/profile_remote_data_source.dart
+++ lib/features/profile/data/datasource/profile_remote_data_source.dart
@@ -1,8 +1,24 @@
-import 'package:crosscheck/features/profile/data/models/data/profile_model.dart';
 import 'package:crosscheck/features/profile/data/models/response/profile_response_model.dart';
+import 'package:http/http.dart';
 
 abstract class ProfileRemoteDataSource {
   
   Future<ProfileResponseModel> getProfile({required String token});
 
+}
+
+class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
+
+  final Client client;
+
+  ProfileRemoteDataSourceImpl({
+    required this.client
+  });
+
+  @override
+  Future<ProfileResponseModel> getProfile({required String token}) {
+    // TODO: implement getProfile
+    throw UnimplementedError();
+  }
+  
 }
