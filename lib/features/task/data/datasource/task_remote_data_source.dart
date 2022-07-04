752491ca77abdaf8f7c7f07dc6ac761ebbd9ce77
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-07-04 13:26:53 +0700

    Task: create task remote data source

--- lib/features/task/data/datasource/task_remote_data_source.dart
+++ lib/features/task/data/datasource/task_remote_data_source.dart
@@ -1,7 +1,25 @@
+
 import 'package:crosscheck/features/task/data/models/response/task_response_model.dart';
+import 'package:http/http.dart';
 
 abstract class TaskRemoteDataSource {
   
   Future<TaskResponseModel> getHistory({required String token});
+
+}
+
+class TaskRemoteDataSourceImpl implements TaskRemoteDataSource {
+
+  final Client client;
+
+  TaskRemoteDataSourceImpl({
+    required this.client
+  });
+  
+  @override
+  Future<TaskResponseModel> getHistory({required String token}) {
+    // TODO: implement getHistory
+    throw UnimplementedError();
+  }
 
 }
