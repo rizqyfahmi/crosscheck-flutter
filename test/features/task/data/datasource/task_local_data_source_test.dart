72c7b6071e6c91d8b42c17d61f68ffdbdd47e7a1
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-07-16 17:27:37 +0700

    Test: create test case for local data source of get task by date

--- test/features/task/data/datasource/task_local_data_source_test.dart
+++ test/features/task/data/datasource/task_local_data_source_test.dart
@@ -121,10 +121,20 @@ void main() {
 
   test("Should get cache monthly task properly", () async {
     final time = DateTime(2022, 7);
-    final utils = Utils();
-    final expected = utils.getMonthlyTaskModel(time: time);
+    final mocks = [
+      MonthlyTaskModel(id: "01", date: DateTime(2022, 6, 15), total: 5),
+      MonthlyTaskModel(id: "02", date: DateTime(2022, 7, 1), total: 8),
+      MonthlyTaskModel(id: "03", date: DateTime(2022, 7, 8), total: 3),
+      MonthlyTaskModel(id: "04", date: DateTime(2022, 7, 25), total: 7),
+      MonthlyTaskModel(id: "05", date: DateTime(2022, 8, 5), total: 2),
+    ];
+    final expected = [
+      MonthlyTaskModel(id: "02", date: DateTime(2022, 7, 1), total: 8),
+      MonthlyTaskModel(id: "03", date: DateTime(2022, 7, 8), total: 3),
+      MonthlyTaskModel(id: "04", date: DateTime(2022, 7, 25), total: 7),
+    ];
     when(mockMonthlyTaskBox.isOpen).thenReturn(true);
-    when(mockMonthlyTaskBox.values.toList()).thenReturn(expected);
+    when(mockMonthlyTaskBox.values.toList()).thenReturn(mocks);
     
     final result = await taskLocalDataSource.getCacheMonthlyTask(time);
 
@@ -164,5 +174,41 @@ void main() {
     ));
     verify(mockMonthlyTaskBox.isOpen);
     verifyNever(mockMonthlyTaskBox.deleteAll(any));
+  });
+
+  /*--------------------------------------------------- Get Task by Date ---------------------------------------------------*/
+  test("Should get task by date properly", () async {
+    final time = DateTime(2022, 7, 3);
+    final mocks = [
+      TaskModel(id: "01", title: "hello title 1", description: "hello description 1", start: DateTime(2022, 7, 1), end: DateTime(2022, 7, 3), isAllDay: false, alerts: const []),
+      TaskModel(id: "02", title: "hello title 2", description: "hello description 2", start: DateTime(2022, 7, 1), end: DateTime(2022, 7, 5), isAllDay: false, alerts: const []),
+      TaskModel(id: "03", title: "hello title 3", description: "hello description 3", start: DateTime(2022, 7, 3), end: DateTime(2022, 7, 6), isAllDay: false, alerts: const []),
+      TaskModel(id: "04", title: "hello title 4", description: "hello description 4", start: DateTime(2022, 7, 4), end: DateTime(2022, 7, 10), isAllDay: false, alerts: const []),
+      TaskModel(id: "05", title: "hello title 5", description: "hello description 5", start: DateTime(2022, 7, 6), end: DateTime(2022, 7, 10), isAllDay: false, alerts: const []),
+    ];
+    final expected = [
+      TaskModel(id: "01", title: "hello title 1", description: "hello description 1", start: DateTime(2022, 7, 1), end: DateTime(2022, 7, 3), isAllDay: false, alerts: const []),
+      TaskModel(id: "02", title: "hello title 2", description: "hello description 2", start: DateTime(2022, 7, 1), end: DateTime(2022, 7, 5), isAllDay: false, alerts: const []),
+      TaskModel(id: "03", title: "hello title 3", description: "hello description 3", start: DateTime(2022, 7, 3), end: DateTime(2022, 7, 6), isAllDay: false, alerts: const []),
+    ];
+    when(mockBox.isOpen).thenReturn(true);
+    when(mockBox.values.toList()).thenReturn(mocks);
+    
+    final result = await taskLocalDataSource.getCachedTaskByDate(time);
+
+    expect(result, expected);
+    verify(mockBox.isOpen);
+    verify(mockBox.values.toList());
+  });
+
+  test("Should returns empty task when get cached task by date where box is not open", () async {
+    final time = DateTime(2022, 7, 3);
+    when(mockBox.isOpen).thenReturn(false);
+
+    final result = await taskLocalDataSource.getCachedTaskByDate(time);
+
+    expect(result, []);
+    verify(mockBox.isOpen);
+    verifyNever(mockBox.values.toList());
   });
 }
