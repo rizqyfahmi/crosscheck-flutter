8b95163ad63fa6e4108feff8c94834fe6f871e14
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-07-20 19:18:34 +0700

    Test: refactor test case for bloc of get initial task by date

--- test/features/task/domain/usecase/get_initial_task_by_date_usecase_test.dart
+++ test/features/task/domain/usecase/get_initial_task_by_date_usecase_test.dart
@@ -82,6 +82,27 @@ void main() {
       MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
       MonthlyTaskEntity(id: "003", date: time.add(const Duration(days: 2)), total: 5),
     ];
+    final CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: mockedMonthlyTaskEntities, taskEntities: const []);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => Right(mockedMonthlyTaskEntities));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => const Left(ServerFailure(message: Failure.generalError, data: [])));
+
+    final result = await getInitialTaskByDateUsecase(time);
+    
+    expect(result, Left(ServerFailure(message: Failure.generalError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns ServerFailure that contains combine task entity as its data when getTaskByDate returns ServerFailure with data", () async {
+    final time = DateTime(2022, 7, 16);
+    final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
+      MonthlyTaskEntity(id: "001", date: time, total: 2),
+      MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
+      MonthlyTaskEntity(id: "003", date: time.add(const Duration(days: 2)), total: 5),
+    ];
     final List<TaskEntity> mockedTaskEntities = [
       TaskEntity(id: "001", title: "Hello title", description: "Hello description", start: time.add(const Duration(hours: 6)), end: time.add(const Duration(hours: 8)), isAllDay: false, alerts: const []),
       TaskEntity(id: "002", title: "Hello title 2", description: "Hello description 2", start: time.add(const Duration(hours: 9)), end: time.add(const Duration(hours: 10)), isAllDay: false, alerts: const []),
@@ -101,6 +122,27 @@ void main() {
   });
 
   test("Should returns CacheFailure that contains combine task entity as its data when getTaskByDate returns CacheFailure", () async {
+    final time = DateTime(2022, 7, 16);
+    final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
+      MonthlyTaskEntity(id: "001", date: time, total: 2),
+      MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
+      MonthlyTaskEntity(id: "003", date: time.add(const Duration(days: 2)), total: 5),
+    ];
+    final CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: mockedMonthlyTaskEntities, taskEntities: const []);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => Right(mockedMonthlyTaskEntities));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+
+    final result = await getInitialTaskByDateUsecase(time);
+    
+    expect(result, Left(CacheFailure(message: Failure.cacheError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns CacheFailure that contains combine task entity as its data when getTaskByDate returns CacheFailure with data", () async {
     final time = DateTime(2022, 7, 16);
     final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
       MonthlyTaskEntity(id: "001", date: time, total: 2),
@@ -127,6 +169,26 @@ void main() {
 
   test("Should returns ServerFailure that contains combine task entity as its data when getMonthlyTask returns ServerFailure", () async {
     final time = DateTime(2022, 7, 16);
+    final List<TaskEntity> mockedTaskEntities = [
+      TaskEntity(id: "001", title: "Hello title", description: "Hello description", start: time.add(const Duration(hours: 6)), end: time.add(const Duration(hours: 8)), isAllDay: false, alerts: const []),
+      TaskEntity(id: "002", title: "Hello title 2", description: "Hello description 2", start: time.add(const Duration(hours: 9)), end: time.add(const Duration(hours: 10)), isAllDay: false, alerts: const []),
+    ];
+    final CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: const [], taskEntities: mockedTaskEntities);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => const Left(ServerFailure(message: Failure.generalError, data: [])));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => Right(mockedTaskEntities));
+
+    final result = await getInitialTaskByDateUsecase(time);
+    
+    expect(result, Left(ServerFailure(message: Failure.generalError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns ServerFailure that contains combine task entity as its data when getMonthlyTask returns ServerFailure with data", () async {
+    final time = DateTime(2022, 7, 16);
     final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
       MonthlyTaskEntity(id: "001", date: time, total: 2),
       MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
@@ -151,6 +213,26 @@ void main() {
   });
 
   test("Should returns CacheFailure that contains combine task entity as its data when getMonthlyTask returns CacheFailure", () async {
+    final time = DateTime(2022, 7, 16);
+    final List<TaskEntity> mockedTaskEntities = [
+      TaskEntity(id: "001", title: "Hello title", description: "Hello description", start: time.add(const Duration(hours: 6)), end: time.add(const Duration(hours: 8)), isAllDay: false, alerts: const []),
+      TaskEntity(id: "002", title: "Hello title 2", description: "Hello description 2", start: time.add(const Duration(hours: 9)), end: time.add(const Duration(hours: 10)), isAllDay: false, alerts: const []),
+    ];
+    final CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: const [], taskEntities: mockedTaskEntities);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => Right(mockedTaskEntities));
+
+    final result = await getInitialTaskByDateUsecase(time);
+    
+    expect(result, Left(CacheFailure(message: Failure.cacheError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns CacheFailure that contains combine task entity as its data when getMonthlyTask returns CacheFailure with data", () async {
     final time = DateTime(2022, 7, 16);
     final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
       MonthlyTaskEntity(id: "001", date: time, total: 2),
@@ -177,6 +259,22 @@ void main() {
 
   test("Should returns UnexpectedFailure that contains combine task entity as its data when getMonthlyTask and getTaskByDate returns Failure", () async {
     final time = DateTime(2022, 7, 16);
+    const CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: [], taskEntities: []);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+
+    final result = await getInitialTaskByDateUsecase(time);
+    
+    expect(result, const Left(UnexpectedFailure(message: Failure.generalError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns UnexpectedFailure that contains combine task entity as its data when getMonthlyTask and getTaskByDate returns Failure with data", () async {
+    final time = DateTime(2022, 7, 16);
     final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
       MonthlyTaskEntity(id: "001", date: time, total: 2),
       MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
@@ -190,15 +288,14 @@ void main() {
 
     when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
     when(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => Left(CacheFailure(message: Failure.cacheError, data: mockedMonthlyTaskEntities)));
-    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => Right(mockedTaskEntities));
+    when(mockTaskRepository.getTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => Left(CacheFailure(message: Failure.cacheError, data: mockedTaskEntities)));
 
     final result = await getInitialTaskByDateUsecase(time);
     
-    expect(result, Left(CacheFailure(message: Failure.cacheError, data: expectedResult)));
+    expect(result, Left(UnexpectedFailure(message: Failure.generalError, data: expectedResult)));
     verify(mockAuthenticationRepository.getToken());
     verify(mockTaskRepository.getTaskByDate(token: Utils.token, time: time));
     verify(mockTaskRepository.getMonthlyTask(token: Utils.token, time: time));
   });
-
 
 }
--- test/features/task/domain/usecase/get_refresh_task_by_date_usecase_test.dart
+++ test/features/task/domain/usecase/get_refresh_task_by_date_usecase_test.dart
@@ -196,4 +196,45 @@ void main() {
     verify(mockTaskRepository.getRefreshTaskByDate(token: Utils.token, time: time));
   });
 
+  test("Should returns UnexpectedFailure that contains combine task entity as its data when getMonthlyTask and getTaskByDate returns Failure", () async {
+    final time = DateTime(2022, 7, 16);
+    const CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: [], taskEntities: []);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getRefreshMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+    when(mockTaskRepository.getRefreshTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => const Left(CacheFailure(message: Failure.cacheError, data: [])));
+
+    final result = await getRefreshTaskByDateUsecase(time);
+    
+    expect(result, const Left(UnexpectedFailure(message: Failure.generalError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getRefreshTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getRefreshMonthlyTask(token: Utils.token, time: time));
+  });
+
+  test("Should returns UnexpectedFailure that contains combine task entity as its data when getMonthlyTask and getTaskByDate returns Failure with data", () async {
+    final time = DateTime(2022, 7, 16);
+    final List<MonthlyTaskEntity> mockedMonthlyTaskEntities = [
+      MonthlyTaskEntity(id: "001", date: time, total: 2),
+      MonthlyTaskEntity(id: "002", date: time.add(const Duration(days: 1)), total: 1),
+      MonthlyTaskEntity(id: "003", date: time.add(const Duration(days: 2)), total: 5),
+    ];
+    final List<TaskEntity> mockedTaskEntities = [
+      TaskEntity(id: "001", title: "Hello title", description: "Hello description", start: time.add(const Duration(hours: 6)), end: time.add(const Duration(hours: 8)), isAllDay: false, alerts: const []),
+      TaskEntity(id: "002", title: "Hello title 2", description: "Hello description 2", start: time.add(const Duration(hours: 9)), end: time.add(const Duration(hours: 10)), isAllDay: false, alerts: const []),
+    ];
+    final CombineTaskEntity expectedResult = CombineTaskEntity(monthlyTaskEntities: mockedMonthlyTaskEntities, taskEntities: mockedTaskEntities);
+
+    when(mockAuthenticationRepository.getToken()).thenAnswer((_) async => const Right(AuthenticationEntity(token: Utils.token)));
+    when(mockTaskRepository.getRefreshMonthlyTask(token: Utils.token, time: time)).thenAnswer((_) async => Left(CacheFailure(message: Failure.cacheError, data: mockedMonthlyTaskEntities)));
+    when(mockTaskRepository.getRefreshTaskByDate(token: Utils.token, time: time)).thenAnswer((_) async => Left(CacheFailure(message: Failure.cacheError, data: mockedTaskEntities)));
+
+    final result = await getRefreshTaskByDateUsecase(time);
+    
+    expect(result, Left(UnexpectedFailure(message: Failure.generalError, data: expectedResult)));
+    verify(mockAuthenticationRepository.getToken());
+    verify(mockTaskRepository.getRefreshTaskByDate(token: Utils.token, time: time));
+    verify(mockTaskRepository.getRefreshMonthlyTask(token: Utils.token, time: time));
+  });
+
 }
